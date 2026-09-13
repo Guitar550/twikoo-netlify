415de6c3337a0f0b0708d3a6aa65a7e3d9b94fe5
Guitar550 <63825076+Guitar550@users.noreply.github.com>
2026-09-13 18:55:14 +0800

    Add CORS headers to fix cross-origin issues

--- netlify/functions/twikoo.js
+++ netlify/functions/twikoo.js
@@ -1,1 +1,27 @@
-exports.handler = require('twikoo-netlify').handler
+const twikooHandler = require('twikoo-netlify').handler;
+
+exports.handler = async (event, context) => {
+      // 处理 OPTIONS 预检请求
+      if (event.httpMethod === 'OPTIONS') {
+                return {
+                              statusCode: 200,
+                              headers: {
+                                                'Access-Control-Allow-Origin': '*',
+                                                'Access-Control-Allow-Headers': 'Content-Type',
+                                                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+                              },
+                              body: ''
+                };
+      }
+
+      // 调用原始的 Twikoo handler
+      const response = await twikooHandler(event, context);
+
+      // 添加 CORS 头
+      response.headers = response.headers || {};
+      response.headers['Access-Control-Allow-Origin'] = '*';
+      response.headers['Access-Control-Allow-Headers'] = 'Content-Type';
+      response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS';
+
+      return response;
+};
