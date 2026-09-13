4ea211d9d61e8cb42920388c6a017724308e85a3
Guitar550 <63825076+Guitar550@users.noreply.github.com>
2026-09-13 19:11:50 +0800

    Update twikoo.js

--- netlify/functions/twikoo.js
+++ netlify/functions/twikoo.js
@@ -1,23 +1,30 @@
- exports.handler = (event, context, callback) => {
-    const CORS_HEADERS = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        'Access-Control-Max-Age': '86400'
-    };
+ const twikoo = require('twikoo-netlify');
 
+const CORS_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+    'Access-Control-Max-Age': '86400'
+};
+
+exports.handler = async (event, context) => {
     if (event.httpMethod === 'OPTIONS') {
-        callback(null, {
+        return {
             statusCode: 200,
             headers: CORS_HEADERS,
             body: ''
-        });
-        return;
+        };
     }
 
-    callback(null, {
-        statusCode: 200,
-        headers: CORS_HEADERS,
-        body: JSON.stringify({ message: 'Test CORS', method: event.httpMethod })
-    });
+    try {
+        const response = await twikoo.handler(event, context);
+        response.headers = Object.assign({}, response.headers, CORS_HEADERS);
+        return response;
+    } catch (error) {
+        return {
+            statusCode: 500,
+            headers: CORS_HEADERS,
+            body: JSON.stringify({ error: error.message, stack: error.stack ? error.stack.substring(0, 500) : '' })
+        };
+    }
 };
